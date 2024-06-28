ad84155e2b0a4e12805a4e284baa80fbf8a49f3f
PeRSiK <152921092+PeRSiKbest@users.noreply.github.com>
2024-06-28 17:58:22 +0300

    Add files via upload

---
+++ 07-06-dz/Books.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string author)
+    {
+        Title = title;
+        Author = author;
+    }
+
+    public override string ToString()
+    {
+        return $"{Title} by {Author}";
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Book otherBook)
+        {
+            return this.Title == otherBook.Title && this.Author == otherBook.Author;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return (Title, Author).GetHashCode();
+    }
+
+    public static bool operator ==(Book book1, Book book2)
+    {
+        if (ReferenceEquals(book1, null))
+            return ReferenceEquals(book2, null);
+        return book1.Equals(book2);
+    }
+
+    public static bool operator !=(Book book1, Book book2)
+    {
+        return !(book1 == book2);
+    }
+}
+
+public class ReadingList
+{
+    private List<Book> books = new List<Book>();
+
+    public int Count
+    {
+        get { return books.Count; }
+    }
+
+    public Book this[int index]
+    {
+        get { return books[index]; }
+        set { books[index] = value; }
+    }
+
+    public void AddBook(Book book)
+    {
+        if (!books.Contains(book))
+        {
+            books.Add(book);
+            Console.WriteLine($"{book} была добавлена в список для чтения.");
+        }
+        else
+        {
+            Console.WriteLine($"{book} уже есть в списке для чтения.");
+        }
+    }
+
+    public void RemoveBook(Book book)
+    {
+        if (books.Contains(book))
+        {
+            books.Remove(book);
+            Console.WriteLine($"{book} была удалена со списка для чтения.");
+        }
+        else
+        {
+            Console.WriteLine($"{book} не состоит в списке для чтения.");
+        }
+    }
+
+    public bool Contains(Book book)
+    {
+        return books.Contains(book);
+    }
+
+    public static ReadingList operator +(ReadingList readingList, Book book)
+    {
+        readingList.AddBook(book);
+        return readingList;
+    }
+
+    public static ReadingList operator -(ReadingList readingList, Book book)
+    {
+        readingList.RemoveBook(book);
+        return readingList;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ReadingList readingList = new ReadingList();
+
+        while (true)
+        {
+            Console.WriteLine("Меню:");
+            Console.WriteLine("1. Добавить книгу");
+            Console.WriteLine("2. Удалить книгу");
+            Console.WriteLine("3. Проверить, есть ли книга в списке для чтения");
+            Console.WriteLine("4. Показать все книги со списка");
+            Console.WriteLine("0. Выход");
+            Console.Write("Выберите действие: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    AddBookToList(readingList);
+                    break;
+                case 2:
+                    RemoveBookFromList(readingList);
+                    break;
+                case 3:
+                    CheckBookInList(readingList);
+                    break;
+                case 4:
+                    ShowAllBooks(readingList);
+                    break;
+                case 0:
+                    return;
+                default:
+                    Console.WriteLine("Неправильный выбор, попробуйте снова.");
+                    break;
+            }
+        }
+    }
+
+    static void AddBookToList(ReadingList readingList)
+    {
+        Console.Write("Введите название книги: ");
+        string title = Console.ReadLine();
+        Console.Write("Введите автора книги: ");
+        string author = Console.ReadLine();
+        Book book = new Book(title, author);
+        readingList += book;
+    }
+
+    static void RemoveBookFromList(ReadingList readingList)
+    {
+        Console.Write("Введите название книги, подлежащей удалению: ");
+        string title = Console.ReadLine();
+        Console.Write("Введите автора книги, подлежащей удалению: ");
+        string author = Console.ReadLine();
+        Book book = new Book(title, author);
+        readingList -= book;
+    }
+
+    static void CheckBookInList(ReadingList readingList)
+    {
+        Console.Write("Введите название книги для проверки: ");
+        string title = Console.ReadLine();
+        Console.Write("Введите автора книги для проверки: ");
+        string author = Console.ReadLine();
+        Book book = new Book(title, author);
+        bool isInList = readingList.Contains(book);
+        Console.WriteLine($"Есть ли '{book}' в списке для чтения? {isInList}");
+    }
+
+    static void ShowAllBooks(ReadingList readingList)
+    {
+        Console.WriteLine("Книги в списке для чтения:");
+        for (int i = 0; i < readingList.Count; i++)
+        {
+            Console.WriteLine($"Книга {i + 1}: {readingList[i]}");
+        }
+    }
+}
---
+++ 07-06-dz/Journals.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+public class Journal
+{
+    private int _numberOfEmployees;
+
+    public int NumberOfEmployees
+    {
+        get { return _numberOfEmployees; }
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Количество работников не может быть меньше 0.");
+            _numberOfEmployees = value;
+        }
+    }
+
+    public Journal(int numberOfEmployees)
+    {
+        NumberOfEmployees = numberOfEmployees;
+    }
+
+    public static Journal operator +(Journal journal, int employees)
+    {
+        return new Journal(journal.NumberOfEmployees + employees);
+    }
+
+    public static Journal operator -(Journal journal, int employees)
+    {
+        return new Journal(journal.NumberOfEmployees - employees);
+    }
+
+    public static bool operator ==(Journal journal1, Journal journal2)
+    {
+        if (ReferenceEquals(journal1, null))
+            return ReferenceEquals(journal2, null);
+
+        return journal1.Equals(journal2);
+    }
+
+    public static bool operator !=(Journal journal1, Journal journal2)
+    {
+        return !(journal1 == journal2);
+    }
+
+    public static bool operator <(Journal journal1, Journal journal2)
+    {
+        return journal1.NumberOfEmployees < journal2.NumberOfEmployees;
+    }
+
+    public static bool operator >(Journal journal1, Journal journal2)
+    {
+        return journal1.NumberOfEmployees > journal2.NumberOfEmployees;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is Journal otherJournal)
+            return this.NumberOfEmployees == otherJournal.NumberOfEmployees;
+
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return NumberOfEmployees.GetHashCode();
+    }
+}
+
+class Journals
+{
+    static void Main()
+    {
+        Console.Write("Введите к-во работников для 1ого журнала: ");
+        int employees1 = Convert.ToInt32(Console.ReadLine());
+        Journal journal1 = new Journal(employees1);
+
+        Console.Write("Введите к-во работников для 2ого журнала: ");
+        int employees2 = Convert.ToInt32(Console.ReadLine());
+        Journal journal2 = new Journal(employees2);
+
+        Console.WriteLine($"Журнал 1: {journal1.NumberOfEmployees} работника(ов)");
+        Console.WriteLine($"Журнал 2: {journal2.NumberOfEmployees} работника(ов)");
+
+        Console.WriteLine($"Журнал 1 == Журнал 2: {journal1 == journal2}");
+        Console.WriteLine($"Журнал 1 != Журнал 2: {journal1 != journal2}");
+        Console.WriteLine($"Журнал 1 < Журнал 2: {journal1 < journal2}");
+        Console.WriteLine($"Журнал 1 > Журнал 2: {journal1 > journal2}");
+
+        Console.Write("Введите к-во работников чтоб ДОБАВИТЬ их к 1ому журналу: ");
+        int addEmployees1 = Convert.ToInt32(Console.ReadLine());
+        journal1 += addEmployees1;
+        Console.WriteLine($"Журнал 1 теперь имеет: {journal1.NumberOfEmployees} работника(ов)");
+
+        Console.Write("Введите к-во работников чтоб ОТНЯТЬ их у 2ого журнала: ");
+        int subtractEmployees2 = Convert.ToInt32(Console.ReadLine());
+        journal2 -= subtractEmployees2;
+        Console.WriteLine($"Журнал 2 теперь имеет: {journal2.NumberOfEmployees} работника(ов)");
+
+        Console.WriteLine($"Журнал 1 == Журнал 2: {journal1 == journal2}");
+    }
+}
