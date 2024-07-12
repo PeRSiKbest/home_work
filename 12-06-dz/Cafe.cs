75fb739129adebad97561a6f8b527e1a19a25628
PeRSiK <152921092+PeRSiKbest@users.noreply.github.com>
2024-07-12 18:36:52 +0300

    Add files via upload

---
+++ 12-06-dz/Cafe.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// Баз. класс
+public class CafeEmployee
+{
+    public string Name { get; set; } //Имя
+    public string Position { get; set; } //Должность
+
+    public CafeEmployee(string name, string position)
+    {
+        Name = name;
+        Position = position;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Position})";
+    }
+}
+
+// Разные работники
+public class Barista : CafeEmployee
+{
+    public Barista(string name) : base(name, "Barista") { }
+}
+
+public class Chef : CafeEmployee
+{
+    public Chef(string name) : base(name, "Chef") { }
+}
+
+public class Waiter : CafeEmployee
+{
+    public Waiter(string name) : base(name, "Waiter") { }
+}
+
+// Кафе
+public class Cafe : IEnumerable<CafeEmployee>
+{
+    private List<CafeEmployee> employees = new List<CafeEmployee>();
+
+    public void AddEmployee(CafeEmployee employee)
+    {
+        employees.Add(employee);
+    }
+
+    public IEnumerator<CafeEmployee> GetEnumerator()
+    {
+        return employees.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Cafe cafe = new Cafe();
+
+        // Работники
+        cafe.AddEmployee(new Barista("Alice"));
+        cafe.AddEmployee(new Chef("Bob"));
+        cafe.AddEmployee(new Waiter("Charlie"));
+
+        foreach (var employee in cafe)
+        {
+            Console.WriteLine(employee);
+        }
+    }
+}
---
+++ 12-06-dz/Oceanarium.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class OceanInhabitant
+{
+    public string Name { get; set; } //Имя 
+    public string Species { get; set; } // Вид
+
+    public OceanInhabitant(string name, string species)
+    {
+        Name = name;
+        Species = species;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Species})";
+    }
+}
+
+// Рибка
+public class Fish : OceanInhabitant
+{
+    public Fish(string name) : base(name, "Fish") { }
+}
+
+//Акулка
+public class Shark : OceanInhabitant
+{
+    public Shark(string name) : base(name, "Shark") { }
+}
+
+//Дельфин
+public class Dolphin : OceanInhabitant
+{
+    public Dolphin(string name) : base(name, "Dolphin") { }
+}
+
+// Океанариум
+public class Oceanarium : IEnumerable<OceanInhabitant>
+{
+    private List<OceanInhabitant> inhabitants = new List<OceanInhabitant>();
+
+    public void AddInhabitant(OceanInhabitant inhabitant)
+    {
+        inhabitants.Add(inhabitant);
+    }
+
+    public IEnumerator<OceanInhabitant> GetEnumerator()
+    {
+        return inhabitants.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Oceanarium oceanarium = new Oceanarium();
+
+        // Жители
+        oceanarium.AddInhabitant(new Fish("Nemo"));
+        oceanarium.AddInhabitant(new Shark("Jaws"));
+        oceanarium.AddInhabitant(new Dolphin("Flipper"));
+
+        foreach (var inhabitant in oceanarium)
+        {
+            Console.WriteLine(inhabitant);
+        }
+    }
+}
