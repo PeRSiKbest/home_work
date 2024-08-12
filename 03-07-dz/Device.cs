6617750954f9f8e793ee23d5fac422ad971511f5
PeRSiK <152921092+PeRSiKbest@users.noreply.github.com>
2024-08-12 21:15:35 +0300

    Add files via upload

---
+++ 03-07-dz/Device.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+public class Device
+{
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public decimal Price { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} ({Manufacturer}) - ${Price}";
+    }
+}
---
+++ 03-07-dz/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Device[] array1 = new Device[]
+        {
+            new Device { Name = "Device1", Manufacturer = "ManufacturerA", Price = 100 },
+            new Device { Name = "Device2", Manufacturer = "ManufacturerB", Price = 200 },
+            new Device { Name = "Device3", Manufacturer = "ManufacturerC", Price = 300 }
+        };
+
+        Device[] array2 = new Device[]
+        {
+            new Device { Name = "Device4", Manufacturer = "ManufacturerB", Price = 150 },
+            new Device { Name = "Device5", Manufacturer = "ManufacturerC", Price = 250 },
+            new Device { Name = "Device6", Manufacturer = "ManufacturerD", Price = 350 }
+        };
+
+        // Разница массивов
+        var difference = array1.Except(array2, new DeviceComparer()).ToArray();
+
+        // Пересечение 
+        var intersection = array1.Intersect(array2, new DeviceComparer()).ToArray();
+
+        // Обьединение
+        var union = array1.Union(array2, new DeviceComparer()).ToArray();
+
+        // Результаты
+        Console.WriteLine("Difference(уникальные):");
+        foreach (var device in difference)
+        {
+            Console.WriteLine(device);
+        }
+
+        Console.WriteLine("\nIntersection(общие):");
+        foreach (var device in intersection)
+        {
+            Console.WriteLine(device);
+        }
+
+        Console.WriteLine("\nUnion(все, без повторов):");
+        foreach (var device in union)
+        {
+            Console.WriteLine(device);
+        }
+    }
+}
+
+public class DeviceComparer : IEqualityComparer<Device>
+{
+    public bool Equals(Device x, Device y)
+    {
+        if (x == null || y == null) return false;
+        return x.Manufacturer == y.Manufacturer;
+    }
+
+    public int GetHashCode(Device obj)
+    {
+        return obj.Manufacturer.GetHashCode();
+    }
+}
